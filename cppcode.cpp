38f4c3be7357184ff2a5a9ca43f34fd36afbcda5
dhaval447 <dhaval.patel9426686447@gmail.com>
2022-05-16 01:28:37 -0700

    resolved conflict

--- cppcode.cpp
+++ cppcode.cpp
@@ -7,6 +7,5 @@ int main(){
 	std::cout << "Cpp code for git practice" << std::endl;
 	std::cout << "added line 1 after making a remote repo online " << std::endl;
 	std::cout << "added line2 after clonning for ssh" << std::endl;
-	std::cout << "added feature2 from dhaval447 " << endl;
 	return 0;
  }
