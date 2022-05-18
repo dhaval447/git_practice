be6439a0fcba8ed5451cd85389423adbcc22f5de
dhaval447 <dhaval.patel7@yahoo.in>
2022-05-18 09:12:44 -0700

    added std::endl

--- cppcode.cpp
+++ cppcode.cpp
@@ -11,7 +11,7 @@ int main(){
 	std::cout << "Cpp code for git practice" << std::endl;
 	std::cout << "added line 1 after making a remote repo online " << std::endl;
 	std::cout << "added line2 after clonning for ssh" << std::endl;
-	std::cout << "added feature1 from dhaval491" << endl;
+	std::cout << "added feature1 from dhaval491" << std::ndl;
 	print_branch();
 	std::cout << "added feature2 better than dhaval447 by dhaval491" << end;
 	return 0;
