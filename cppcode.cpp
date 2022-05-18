58f9af82458516ecbac6cbd3cc3afd680104fcea
dhaval447 <dhaval.patel9426686447@gmail.com>
2022-05-18 08:42:37 -0700

    added a function to print banch and details

--- cppcode.cpp
+++ cppcode.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
+void print_branch(){
+	cout << "changes from dhaval491" << endl;
+}
+
 int main(){
 	std::cout << "Branch = master" << endl;
 	std::cout << "Cpp code for git practice" << std::endl;
 	std::cout << "added line 1 after making a remote repo online " << std::endl;
 	std::cout << "added line2 after clonning for ssh" << std::endl;
 	std::cout << "added feature1 from dhaval491" << endl;
+	print_branch();
 	std::cout << "added feature2 better than dhaval447 by dhaval491" << end;
 	return 0;
  }
