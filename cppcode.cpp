2777af21e6c52c532dd327de025d987236cb1a5b
dhaval447 <dhaval.patel7@yahoo.in>
2022-05-18 09:05:14 -0700

    keep local changes while rebasing

--- cppcode.cpp
+++ cppcode.cpp
@@ -12,8 +12,6 @@ int main(){
 	std::cout << "added line 1 after making a remote repo online " << std::endl;
 	std::cout << "added line2 after clonning for ssh" << std::endl;
 	std::cout << "added feature1 from dhaval491" << endl;
-	std::cout << "added feature between f1 and f3" << endl;
-	std::cout << "added a missing feature" << endl;
 	print_branch();
 	std::cout << "added feature2 better than dhaval447 by dhaval491" << end;
 	return 0;
